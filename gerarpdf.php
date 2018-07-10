a8d3d3c5e9e0983f68301d17ec17e161cb6e354a
Felipe8402 <40921674+Felipe8402@users.noreply.github.com>
2018-07-09 23:44:15 -0300

    5.2.1
    Início na utilização do PHP

--- gerarpdf.php
+++ gerarpdf.php
@@ -1,21 +1,62 @@
 <?php
 
-
+// DADOS PESSOAIS
 $fnome= utf8_decode($_POST["Nome"]);
+$femail= utf8_decode($_POST["Email"]);
 $ftelefone= utf8_decode($_POST["Telefone"]);
 $fsexo= utf8_decode($_POST["Sexo"]);
 $festadocivil= utf8_decode($_POST["EstadoCivil"]);
-
-/*
+$fnascimento= utf8_decode($_POST["Nascimento"]);
 $festado= utf8_decode($_POST["Estado"]);
 $fcidade= utf8_decode($_POST["Cidade"]);
 $fcep= utf8_decode($_POST["CEP"]);
 $fendereco= utf8_decode($_POST["Endereco"]);
-$fcurso= utf8_decode($_POST["Curso"]);
-$finstituicao= utf8_decode($_POST["Instituicao"]);
-$fconclusao= utf8_decode($_POST["Conclusao"]);
-$fanoconclusao= utf8_decode($_POST["AnoConclusao"]);
-*/
+
+// FORMAÇÃO PROFISSIONAL 1
+$fcurso1 = utf8_decode($_POST["Curso1"]);
+$finstituicao1 = utf8_decode($_POST["Instituicao1"]);
+$fanoinicial1 = utf8_decode($_POST["AnoInicial1"]);
+$fconclusao1 = utf8_decode($_POST["Conclusao1"]);
+$fanofinal1 = utf8_decode($_POST["AnoFinal1"]);
+$fqualificacoes1 = utf8_decode($_POST["Qualificacoes1"]);
+
+// FORMAÇÃO PROFISSIONAL 2
+$fcurso2 = utf8_decode($_POST["Curso2"]);
+$finstituicao2 = utf8_decode($_POST["Instituicao2"]);
+$fanoinicial2 = utf8_decode($_POST["AnoInicial2"]);
+$fconclusao2 = utf8_decode($_POST["Conclusao2"]);
+$fanofinal2 = utf8_decode($_POST["AnoFinal2"]);
+$fqualificacoes2 = utf8_decode($_POST["Qualificacoes2"]);
+
+// EXPERIÊNCIA PROFISSIONAL 1
+$fempresa1 = utf8_decode($_POST["Empresa1"]);
+$ffuncao1 = utf8_decode($_POST["Funcao1"]);
+$fdataentrada1 = utf8_decode($_POST["DataEntrada1"]);
+$fdatasaida1 = utf8_decode($_POST["DataSaida1"]);
+$fatividadesrealizadas1 = utf8_decode($_POST["AtividadesRealizadas1"]);
+
+// EXPERIÊNCIA PROFISSIONAL 2
+$fempresa2 = utf8_decode($_POST["Empresa2"]);
+$ffuncao2 = utf8_decode($_POST["Funcao2"]);
+$fdataentrada2 = utf8_decode($_POST["DataEntrada2"]);
+$fdatasaida2 = utf8_decode($_POST["DataSaida2"]);
+$fatividadesrealizadas2 = utf8_decode($_POST["AtividadesRealizadas2"]);
+
+
+require('fpdf.php');
+
+$pdf = new FPDF();
+$pdf->AddPage();
+//
+//título e texto
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,"$fnome");
+$pdf->Ln();
+//
+//título e texto
+$pdf->SetFont('Arial','',14);
+$pdf->Cell(60,10,'Endereço de email',0,1,"$fnome");
+$pdf->Ln();
 
 
 
@@ -24,16 +65,14 @@ $fanoconclusao= utf8_decode($_POST["AnoConclusao"]);
 
 
 
-require_once("fpdf/fpdf.php");
- 
-$pdf = new FPDF('L','pt','A4');
-
-//nome
-$pdf->SetFont('Helvetica','I',13);
-$pdf->Cell(40,10,"miimeira página pdf com FPDF");
-
-$pdf->Output("arquivo.pdf","D");
 
 
 
- ?>
+
+
+
+
+
+
+$pdf->Output();
+?>
