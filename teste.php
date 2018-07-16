f6a767b88127b8f2c967f03919e6e17535cabe77
Felipe8402 <40921674+Felipe8402@users.noreply.github.com>
2018-07-15 21:33:43 -0300

    7.3.4
    Correção de bugs, faltando arrumar o UTF-8

--- teste.php
+++ teste.php
@@ -1,54 +1,7 @@
 <?php
-require_once 'dompdf/lib/html5lib/Parser.php';
-require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
-require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
-require_once 'dompdf/src/Autoloader.php';
-Dompdf\Autoloader::register();
 
-
-// reference the Dompdf namespace
-use Dompdf\Dompdf;
-
-// instantiate and use the dompdf class
-
-
-$dompdf = new Dompdf();
-
-/*
-// EXPERIÊNCIA PROFISSIONAL 1
-$fempresa1 = utf8_decode($_GET["Empresa1"]);
-$ffuncao1 = utf8_decode($_GET["Funcao1"]);
-$fdataentrada1 = utf8_decode($_GET["DataEntrada1"]);
-$fdatasaida1 = utf8_decode($_GET["DataSaida1"]);
-$fatividadesrealizadas1 = utf8_decode($_GET["AtividadesRealizadas1"]);
-*/
-
-$fnome= utf8_decode($_POST["nome"]);
-$femail= utf8_decode($_POST["email"]);
-$ftelefone= utf8_decode($_POST["telefone"]);
-$fsexo= utf8_decode($_POST["sexo"]);
-$festadocivil= utf8_decode($_POST["estado_civil"]);
-$fnascimento= utf8_decode($_POST["nascimento"]);
-$festado= utf8_decode($_POST["estado"]);
-$fcidade= utf8_decode($_POST["cidade"]);
-$fcep= utf8_decode($_POST["cep"]);
-$fendereco= utf8_decode($_POST["endereco"]);
-$finstituicao = utf8_decode($_POST["instituicao"]);
-$fanoinicial = utf8_decode($_POST["anoinicial"]);
-$fconclusao = utf8_decode($_POST["conclusao"]);
-$fanofinal = utf8_decode($_POST["anofinal"]);
-$fqualificacoes = utf8_decode($_POST["qualificacoes"]);
-$fobjetivos = utf8_decode($_POST["objetivos"]);
-$fempresa = utf8_decode($_POST["empresa"]);
-$ffuncao = utf8_decode($_POST["funcao"]);
-$fdataentrada = utf8_decode($_POST["dataentrada"]);
-$fdatasaida = utf8_decode($_POST["datasaida"]);
-$fatividades = utf8_decode($_POST["atividades"]);
-$fcurso = utf8_decode($_POST["curso"]);
-
-/* Preparação do documento final
- */
-$documentTemplate = '<!doctype html>
+$html='
+<!doctype html>
 <html>
 
 <head>
@@ -108,9 +61,7 @@ $documentTemplate = '<!doctype html>
 
 </body>
 
-</html>';
-
-
-$dompdf->loadHtml($documentTemplate);
-echo $documentTemplate
+</html>
+';
+echo $html;
 ?>
